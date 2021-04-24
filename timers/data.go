29d727ea53ffda42301dbd4128e3ace89e38fd01
Adam Walker <awalker@virtualmindset.com>
2021-04-24 16:40:36 -0400

    Refactor: Move local data functions to local.go

--- timers/data.go
+++ timers/data.go
@@ -1,12 +1,5 @@
 package timers
 
-import (
-	"encoding/json"
-	"fmt"
-	"os"
-	"path"
-)
-
 type TimerTemplate struct {
 	Name          string
 	TimerType     int
@@ -34,60 +27,10 @@ type Timers struct {
 	PomBeforeLongBreak int
 }
 
-type LocalTimers struct {
-	Timers
-	filePath string
-}
-
 func NewTemplate(name, templateLabel string, timerType, duration int, sb, lb *string) *TimerTemplate {
 	return &TimerTemplate{name, timerType, duration, templateLabel, sb, lb}
 }
 
-func Load(configFolder string) (*LocalTimers, error) {
-	jsonFileName := path.Join(configFolder, "pomo.json")
-	timers := LocalTimers{}
-	timers.filePath = jsonFileName
-	return &timers, timers.Load()
-}
-
 func (t *Timer) Completed() bool {
-	return false // FIXME: so it so
-}
-
-func (t *LocalTimers) Load() error {
-	if jsonFile, err := os.Open(t.filePath); err == nil {
-		defer jsonFile.Close()
-		decoder := json.NewDecoder(jsonFile)
-		err = decoder.Decode(&t.Timers)
-		if err != nil {
-			return fmt.Errorf("timers:Failed to parse config file (%w)", err)
-		}
-		return nil
-	} else if os.IsNotExist(err) {
-		// Config file not found. Create a blank config.
-		t.AutoStartBreaks = true
-		t.DesiredPomsPerDay = 8
-		t.PomBeforeLongBreak = 4
-		ssb, slb := "Short Break", "Long Break"
-		w := NewTemplate("Pom", "", 1, 60*25, &ssb, &slb)
-		sb := NewTemplate(ssb, "", 2, 60*5, nil, nil)
-		lb := NewTemplate(slb, "", 3, 60*15, nil, nil)
-		t.Templates = []*TimerTemplate{w, sb, lb}
-		// Should probably save the config now.
-		return t.Save()
-	} else {
-		return fmt.Errorf("timers:Could not load config file (%w)", err)
-	}
-}
-
-func (t *LocalTimers) Save() error {
-	json.NewEncoder(os.Stdout).Encode(t.Timers)
-	/*if wFile, err := os.Create(jsonFileName); err == nil {
-		defer wFile.Close()
-		enc := json.NewEncoder(wFile)
-		enc.Encode(timers)
-	} else {
-		fmt.Println("Could not right configuration file. ", jsonFileName, err)
-	}*/
-	return nil
+	return false // FIXME: Make it so
 }
---
+++ timers/local.go
@@ -0,0 +1,58 @@
+package timers
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path"
+)
+
+type LocalTimers struct {
+	Timers
+	filePath string
+}
+
+func Load(configFolder string) (*LocalTimers, error) {
+	jsonFileName := path.Join(configFolder, "pomo.json")
+	timers := LocalTimers{}
+	timers.filePath = jsonFileName
+	return &timers, timers.Load()
+}
+
+func (t *LocalTimers) Load() error {
+	if jsonFile, err := os.Open(t.filePath); err == nil {
+		defer jsonFile.Close()
+		decoder := json.NewDecoder(jsonFile)
+		err = decoder.Decode(&t.Timers)
+		if err != nil {
+			return fmt.Errorf("timers:Failed to parse config file (%w)", err)
+		}
+		return nil
+	} else if os.IsNotExist(err) {
+		// Config file not found. Create a blank config.
+		t.AutoStartBreaks = true
+		t.DesiredPomsPerDay = 8
+		t.PomBeforeLongBreak = 4
+		ssb, slb := "Short Break", "Long Break"
+		w := NewTemplate("Pom", "", 1, 60*25, &ssb, &slb)
+		sb := NewTemplate(ssb, "", 2, 60*5, nil, nil)
+		lb := NewTemplate(slb, "", 3, 60*15, nil, nil)
+		t.Templates = []*TimerTemplate{w, sb, lb}
+		// Should probably save the config now.
+		return t.Save()
+	} else {
+		return fmt.Errorf("timers:Could not load config file (%w)", err)
+	}
+}
+
+func (t *LocalTimers) Save() error {
+	json.NewEncoder(os.Stdout).Encode(t.Timers)
+	/*if wFile, err := os.Create(jsonFileName); err == nil {
+		defer wFile.Close()
+		enc := json.NewEncoder(wFile)
+		enc.Encode(timers)
+	} else {
+		fmt.Println("Could not right configuration file. ", jsonFileName, err)
+	}*/
+	return nil
+}
